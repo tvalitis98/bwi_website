648715a76c457953512c6f4b3be32d6f3a5dc3e0
tvalitis98 <tiffany.valitis@gmail.com>
2017-12-12 16:17:10 -0600

    Added info, styling, and the google calendar.

--- www/html/head.php
+++ www/html/head.php
@@ -1,19 +1,20 @@
 <head>
         <link rel="stylesheet" href="style.css">
 </head>
-
+<div class="header" style="background:black; color:white">
 <div style="width:100%; background:black">
 	<ul>
-	  <li><a href="index.php" style="float:left">Building Wide Intelligence Project</a></li>
-	  <li><a href="login.php" style="float:right">Login</a></li>
+	  <li><a class="header" href="index.php" style="float:left">Building Wide Intelligence Project</a></li>
+	  <li><a class="header" href="login.php" style="float:right">Login</a></li>
 	</ul>
 </div>
 <div style="width:100%; background:black">
 	<ul>
-	  <li><a href="public.php#about">About Us</a></li>
-	  <li><a href="public.php#pubs">Publications</a></li>
-	  <li><a href="public.php#research">Research</a></li>
-	  <li><a href="public.php#people">People</a></li>
-	  <li><a href="public.php#contact">Contact Us</a></li>
+	  <li><a class="header" href="public.php#about">About Us</a></li>
+	  <li><a class="header" href="public.php#pubs">Publications</a></li>
+	  <li><a class="header" href="public.php#research">Research</a></li>
+	  <li><a class="header" href="public.php#people">People</a></li>
+	  <li><a class="header" href="public.php#contact">Contact Us</a></li>
 	</ul>
+</div>
 </div>
--- www/html/public.php
+++ www/html/public.php
@@ -1,10 +1,245 @@
 <?php require 'head.php';?>
-<h1 id="about">This is a general document containing information. Headers will auto scroll to the correct portion of the page.</h1>
+<h2 id="about">About Us</h2>
 
-<h1 id="pubs">These are BWI publications.</h1>
+<center><p>The mission the Building-Wide Intelligence Project is to develop fully autonomous mobile robots that can become a permanent part of a building's environment. Working in the intersection of Artificial Intelligence and Robotics, our aim is to create robots that are useful platforms for groundbreaking research, while at the same time are also helpful for humans. Some prominent areas of research include reasoning, planning, human-robot interaction, grounded language learning, and multi-robot coordination.</p></center>
 
-<h1 id="research">This the research the BWI lab conducts.</h1>
+<center><iframe src="https://calendar.google.com/calendar/embed?src=bwi.robots%40gmail.com&ctz=America%2FChicago" style="border: 0" width="800" height="600" frameborder="0" scrolling="no" ></iframe></center>
 
-<h1 id="people">These are the people involved in the BWI project.</h1>
+<h2 id="pubs">Publications</h2>
 
-<h1 id="contact">Contact information.</h1>
+<h4>2018:</h4>
+
+<ul>
+	<li>Suhua Wei, Shiqi Zhang, Jivko Sinapov, Jesse Thomason, and Peter Stone (2018)<br>
+	<b>Robot Behavioral Exploration and Multi-modal Perception using Dynamically Constructed Controllers</b><br>
+	<i>To appear in proceedings of the AAAI Springg Symposium on Integrating Representation, Reasoning, Learning, and Execution for Goal Directed Autonomy, Stanford, CA, Mar. 26-28, 2018.</i><br>
+	</li><br>
+	
+	
+	<li>Jesse Thomason, Jivko Sinapov, Peter Stone, and Raymond J. Mooney (2018)<br>
+	<b>Guiding Exploratory Behaviors for Multi-Modal Grounding of Linguistic Descriptions</b><br>
+	<i>To appear in proceedings of the 32nd Conference of the Association for the Advancement of Artificial Intelligence (AAAI), New Orleans, LA, Feb. 2-7, 2018.</i><br>
+	</i> <a href="https://www.eecs.tufts.edu/~jsinapov/papers/Thomason_AAAI_2018.pdf" target="_blank">[PDF]</a><br>
+	</li>
+</ul>
+
+<h4>2017:</h4>
+
+<ul>
+	
+	
+
+	<li>Jesse Thomason, Aishwarya Padmakumar, Jivko Sinapov, Justin Hart, Peter Stone, Raymond J. Mooney (2017)<br>
+	<b>Opportunistic Active Learning for Grounding Natural Language Descriptions</b><br>	
+	<i>In proceedings of the 1st Annual Conference on Robot Learning (CoRL 2017), Mountain View, California, November 13-15, 2017.</i><br>
+	<a href="" target="_blank">[PDF]</a><br>
+	</li><br>
+
+	<li>Jesse Thomason, Jivko Sinapov, and Raymond J. Mooney (2017)<br>
+	<b>Guiding Interaction Behaviors for Multi-modal Grounded Language Learning</b><br>
+	<i>In proceedings of the 1st Workshop on Language Grounding for Robotics at ACL, Vancouver, Canada, August 3, 2017.</i><br>
+	<a href="http://www.cs.utexas.edu/~jsinapov/papers/Thomason_RoboNLP_ACL_2017.pdf" target="_blank">[PDF]</a><br>
+	</li><br>
+	
+	<li>Santiago Gonzalez, Vijay Chidambaram, Jivko Sinapov, and Peter 
+	Stone (2017)<br>
+	<b>CC-Log: Drastically Reducing Storage Requirements for Robots Using Classification and Compression</b><br>
+	<i>In proceedings of The 9th USENIX Workshop on Hot Topics in Storage and File Systems (HotStorage '17), Santa Clara, CA, July 10-11, 2017.</i><br>
+	<a href="http://www.cs.utexas.edu/~jsinapov/papers/gonzalez_HotStorage2017.pdf" target="_blank">[PDF]</a><br>
+	</li><br>
+	
+	<li>Shiqi Zhang, Yuqian Jiang, Guni Sharon, and Peter Stone (2017)<br>
+	<b>Multirobot Symbolic Planning under Temporal Uncertainty</b><br>
+	<i>In Proceedings of the 16th International Conference on Autonomous Agents and Multiagent Sytems (AAMAS), Sao Paulo, Brazil, May 9-12, 2017.</i><br>
+	<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAMAS17-Zhang.pdf" target="_blank">[PDF]</a><br>
+	</li><br>
+	
+	<li>Piyush Khandelwal and Peter Stone (2017)<br>
+	<b> Multi-Robot Human Guidance: Human Experiments and Multiple Concurrent Requests</b><br>
+	<i>In Proceedings of the 16th International Conference on Autonomous Agents and Multiagent Sytems (AAMAS), Sao Paulo, Brazil, May 9-12, 2017.</i><br>
+	<a href="http://www.google.com/url?q=http%3A%2F%2Fwww.cs.utexas.edu%2F~pstone%2FPapers%2Fbib2html-links%2FAAMAS17-khandelwal.pdf&sa=D&sntz=1&usg=AFQjCNGQ8I80BZN7AY6JUhzFiDEzB2q3xQ" target="_blank">[PDF]</a><br>
+	</li><br>
+	
+
+	
+	<li>Aishwarya Padmakumar, Jesse Thomason, and Raymond J. Mooney (2017)<br>
+	<b>Integrated Learning of Dialog Strategies and Semantic Parsing</b><br>
+	<i>In Proceedings of the 15th Conference of the European Chapter of the Association for Computational Linguistics (EACL 2017), 547--557, Valencia, Spain, April 2017. </i><br>
+	<a href="http://www.cs.utexas.edu/users/ml/papers/padmakumar.eacl17.pdf" target="_blank">[PDF]</a><br>
+	</li><br>
+	
+	<li>Shiqi Zhang, Jivko Sinapov, Suhua Wei, and Peter Stone (2017)<br>
+	<b>Robot Behavioral Exploration and Multimodal Perception using POMDPs</b><br>
+	<i>In proceedings of the AAAI Spring Symposium on "Interactive Multi-Sensory Object Perception for Embodied Agents", San Francisco, CA, March 27-29, 2017. </i><br><a href="http://www.cs.utexas.edu/~jsinapov/papers/zhang_AAAI_SSS2017.pdf" target="_blank">[PDF]</a><br>
+	</li><br>
+	
+	<li>Shiqi Zhang, Piyush Khandelwal, and Peter Stone (2017)<br>
+	<b>Dynamically Constructed (PO)MDPs for Adaptive Robot Planning</b><br>
+	<i>In Proceedings of the 31st AAAI Conference on Artificial Intelligence (AAAI), San Francisco, CA, February, 2017.</i><br>
+	<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAAI17-Zhang.pdf" target="_blank">[PDF]</a><br>
+	</li><br>
+	
+	<li>Piyush Khandelwal, Shiqi Zhang, Jivko Sinapov, Matteo Leonetti, Jesse Thomason, Fangkai Yang, Ilaria Gori, Maxwell Svetlik, Priyanka Khante, Vladimir Lifschitz, J. K. Aggarwal, Ray Mooney, and Peter Stone.<br>
+	<b>BWIBots: A platform for bridging the gap between AI and Human-Robot Interaction research</b><br>
+	<i>International Journal of Robotics Research (in press)</i> <br>
+	<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html/b2hd-IJRR17-khandelwal.html" target="_blank">[LINK]</a> <a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/IJRR17-khandelwal.pdf" target="_blank">[PDF]</a>
+	</li>
+</ul>
+
+<h4>2016:</h4>
+<ul>
+	<li>Jivko Sinapov, Priyanka Khante, Maxwell Svetlik, and Peter Stone.<br>
+	<b>Learning to Order Objects using Haptic and Proprioceptive Exploratory Behaviors</b><br>
+	<i>In proceedings of the 2016 International Joint Conference on Artificial Intelligence (IJCAI), July 2016.</i> <br>
+	<a href="http://www.cs.utexas.edu/~jsinapov/papers/jsinapov_ijcai2016.pdf" target="_blank">[PDF]</a> <a href="http://www.cs.utexas.edu/~jsinapov/slides/jsinapov_slides_IJCAI_2016.pdf" target="_blank">[Slides]</a>
+	</li><br>
+	
+	<li>Jesse Thomason, Jivko Sinapov, Maxwell Svetlik, Peter Stone, and Ray Mooney.<br>
+	<b>Learning Multi-Modal Grounded Linguistic Semantics by Playing I, Spy</b><br>
+	<i>In proceedings of the 2016 International Joint Conference on Artificial Intelligence (IJCAI), July 2016.</i><br> <a href="http://www.cs.utexas.edu/~jsinapov/papers/thomason_ijcai16.pdf" target="_blank">[PDF]</a> <a href="https://youtu.be/jLHzRXPCi_w" target="_blank">[Video]</a>
+	</li><br>
+	
+	<li>Ginevra Gaudioso, Matteo Leonetti, and Peter Stone.<br><b>State Aggregation through Reasoning in Answer Set Programming.</b><br>
+	<i>In IJCAI'16 Workshop on Autonomous Mobile Service Robots, July 2016.</i> <br> <a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/IJCAI-WSR-gaudioso.pdf" target="_blank">[PDF]</a>
+	</li><br>
+	
+	
+	<li>Shih-Yun Lo, Shiqi Zhang, and Peter Stone.<br><b>Integrated Task and Motion Planning for Mobile Service Robots</b><br>
+	<i>In RSS'16 Workshop on Task and Motion Planning, June 2016.</i> <br> <a href="http://www.cs.utexas.edu/users/yunl/publications/Integrated_task_and_motion_planning_for_mobile_service_robots.pdf" target="_blank">[PDF]</a>
+	</li>
+	
+	
+</ul>
+
+
+<h4>2015:</h4>
+<ul>
+	
+	
+	<li>
+		Ilaria Gori, <a href="http://www.cs.utexas.edu/~jsinapov">Jivko Sinapov</a>, Priyanka Khante, <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>, and  J. K. Aggarwal.<br />
+		<b>Robot-centric activity recognition ‘in the wild’</b>.<br />
+		<i>In the Proceedings of the 2015 International Conference on Social Robotics (ICSR)</i>, October 2015.<br />
+		<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/ICSR2015-gori.pdf">[PDF]</a>
+		<br /><br />
+	</li>
+	
+	<li>
+		<a href="http://jessethomason.com/">Jesse Thomason</a>, Shiqi Zhang, <a href="http://www.cs.utexas.edu/~mooney">Raymond Mooney</a>, and <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>.<br />
+		<b>Learning to Interpret Natural Language Commands through Human-Robot Dialog</b>.<br />
+		In <i>Proceedings of the 2015 International Joint Conference on Artificial Intelligence (IJCAI)</i>, July 2015.<br />
+		<a href="https://youtu.be/FL9IhJQOzb8">[Demo Video]</a>
+		<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/IJCAI15-thomason.pdf">[PDF]</a>
+		<br /><br />
+
+	</li>
+	
+	<li>
+		<a href="http://www.cs.utexas.edu/~piyushk">Piyush Khandelwal</a>, <a href="http://www.cs.utexas.edu/~sbarrett">Samuel Barrett</a>, and <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>. <br />
+		<b>Leading the Way: An Efficient Multi-robot Guidance System</b>.<br />
+        In <i>International Conference on Autonomous Agents and Multiagent Systems (AAMAS)</i>, May 2015.<br />
+        <a href="https://www.youtube.com/watch?v=os1BjHgM5ao&amp;feature=youtu.be">[Demo Video]</a><a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAMAS15-khandelwal.pdf">[PDF]</a>
+		<br /><br />
+    </li>
+	
+
+	
+	<li>
+		Shiqi Zhang, Fangkai Yang, <a href="http://www.cs.utexas.edu/~piyushk">Piyush Khandelwal</a>, and <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>.<br />
+		<b>Mobile Robot Planning using Action Language BC with an Abstraction Hierarchy</b>.<br />
+		In <i>Proceedings of the 13th International Conference on Logic Programming and Non-monotonic Reasoning (LPNMR)</i>, September 2015.
+		<a href="https://youtu.be/-QpFj7BbiRU">[Demo Video]</a>
+		<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/LPNMR15-zhang.pdf">[PDF]</a>
+		<br /><br />
+    </li>
+	
+	<li>
+		Shiqi Zhang and <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>.<br />
+		<b>CORPP: Commonsense Reasoning and Probabilistic Planning, as Applied to Dialog with a Mobile Robot</b>.<br />
+		In <i>Proceedings of the 29th Conference on Artificial Intelligence (AAAI)</i>, January 2015.
+		<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAAI15-szhang.pdf">[PDF]</a>
+        <br /><br />
+    </li>
+    
+</ul>
+        
+<h4>2014:</h4>
+
+<ul>
+
+	<li>
+		<a href="http://www.cs.utexas.edu/~piyushk">Piyush Khandelwal</a>, Fangkai Yang, Matteo Leonetti, Vladimir Lifschitz, and <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>.<br />
+		<b>Planning in Action Language BC while Learning Action Costs for Mobile Robots</b>.<br />
+		In <i>International Conference on Automated Planning and Scheduling (ICAPS)</i>, June 2014.
+		<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/ICAPS14-khandelwal.pdf">[PDF]</a>
+        <br /><br />       
+    </li>
+
+    <li>
+		<a href="http://www.cs.utexas.edu/~piyushk">Piyush Khandelwal</a> and <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>.<br />
+        <b>Multi-robot Human Guidance using Topological Graphs</b>. <br />
+        In <i>AAAI Spring 2014 Symposium on Qualitative Representations for Robots (AAAI-SSS)</i>, March 2014.
+        <a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAAISSS14-khandelwal.pdf">[PDF]</a>
+        <br /><br />  
+            
+    </li>
+
+	<li>
+		Fangkai Yang, <a href="http://www.cs.utexas.edu/~piyushk">Piyush Khandelwal</a>, Matteo Leonetti, and <a href="http://www.cs.utexas.edu/~pstone">Peter Stone</a>.<br />
+		<b>Planning in Answer Set Programming while Learning Action Costs for Mobile Robots</b>. <br />
+		In <i>AAAI Spring 2014 Symposium on Knowledge Representation and Reasoning in Robotics (AAAI-SSS)</i>, March 2014.
+		<a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAAISSS14-yang.pdf">[PDF]</a>
+		<br /><br />
+	</li>
+    
+    
+    
+</ul>
+
+<h2 id="research">Research</h2>
+
+<ul>
+	<li><h4>Reasoning and Planning for Mobile Robots</h4>
+	<p>To be deemed intelligent in human-robot coexisting environments, robots need the capability of representing and reasoning with commonsense knowledge. Such knowledge is normally true, but not always. For instance, people prefer coffee in the morning; and office doors are closed during the holidays. We use answer set programming (ASP) and its extensions for commonsense reasoning and partially observable Markov decision processes (POMDPs) for probabilistic planning. The goal of this research is to develop algorithms to enable robots to plan under uncertainty while reason with commonsense knowledge at the same time.</p>
+	<p>Representative Publication: <a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAAI15-szhang.pdf">CORPP: Commonsense Reasoning and Probabilistic Planning, as Applied to Dialog with a Mobile Robot. (AAAI 2015)</a></p>
+	
+	</li>
+	<li><h4>Human-Robot Interaction</h4>
+	<p>
+	Robots are becoming increasingly sophisticated, and are bound to become per-vasive in humans’ every-day lives. To effectively collaborate with humans, it is useful for a robot to understand their activities and intentions automatically. This understanding is especially important in human-robot interaction scenarios: if the robot can properly interpret the behavior of humans, its communication with them will be facilitated, and its ability to interact with them will improve. Therefore, the aim of this research is to develop new methods that would allow a robot to effectively recognize human activities, intentions, etc. and appropriately react to them. 
+	</p>
+	<p>Representative Publication: <a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/ICSR2015-gori.pdf">Robot-centric activity recognition ‘in the wild’. (ICSR 2015)</a></p>
+	</li>
+	
+	
+	<li><h4>Multi-Robot Coordination and Guidance</h4><p>
+	In this research, we demonstrate how individual service robots in a multi-robot system can be temporarily reassigned from their original task to help guide a human from one location to another in the environment. We formulate this multi-robot treatment of the human guidance problem as a Markov Decision Process (MDP), and explore how different MDP planners can be used to solve this problem. Our long term goal for this research is to expand the MDP to a general framework for efficiently interrupting robots performing background service tasks to efficiently aid humans as necessary.
+	</p>
+	<p>Representative Publication: <a href="http://www.cs.utexas.edu/~pstone/Papers/bib2html-links/AAMAS15-khandelwal.pdf">Leading the Way: An Efficient Multi-robot Guidance System (AAMAS 2015)</a></p>
+	</li>
+</ul>
+
+<h2 id="people">People</h2>
+
+<p><b>Principal Investigator (PI): </b><a href="http://www.cs.utexas.edu/~pstone/index.shtml" target="_blank">Peter Stone</a></p>
+
+<p><b>Co-PIs: </b><a href="http://www.cs.utexas.edu/~grauman/" target="_blank">Kristen Grauman</a>, and <a href="https://www.cs.utexas.edu/~mooney/" target="_blank">Ray Mooney</a></p>
+
+<p><b>Affiliated Faculty, Postdoctoral Associates, and Visiting Scientists: </b><a href="http://www.cs.utexas.edu/~vijay/" target="_blank">Vijay Chidambaram</a>, <a href="http://justinhart.net/" target="_blank">Justin Hart</a>, and <a href="https://scholar.google.com/citations?user=Ndp8dmgAAAAJ&hl=en&oi=ao" target="_blank">Garrett Warnell</a></p>
+
+<p><b>Graduate Students: </b><a href="https://www.cs.utexas.edu/~piyushk" target="_blank">Piyush Khandelwal</a>, Priyanka Khante, <a href="http://jessethomason.com/" target="_blank">Jesse Thomason</a>, <a href="http://willxie.com/" target="_blank">William Xie</a>, <a href="http://www.cs.utexas.edu/~aish" target="_blank">Aishwarya Padmakumar</a>, <a href="http://www.cs.utexas.edu/~rf22784/" target="_blank">Rolando Fernandez</a>, and <a href="https://shihyunlo.wordpress.com/" target="_blank">Shih-Yun Lo</a></p>
+
+<p><b>Undergraduate Students: </b>Yuqian Jiang, <a href="https://github.com/katiewasnothere">Kathryn E Baldauf</a>, <a href="https://github.com/nj1407">Nathan John</a>, <a href="http://rdelfin.com/">Ricardo Delfin Garcia</a>, <a href="http://nickwalker.us">Nick Walker</a>, and Benjamin Singer</p>
+
+<p><b>Volunteers: </b><a href="https://github.com/jack-oquin/">Jack O'Quin</a></p>
+
+<p><b>Alumni: </b><a href="http://www.cs.utexas.edu/~fkyang/">Fangkai Yang</a> (Schlumberger Ltd), <a href="https://www.cs.utexas.edu/~matteo" target="_blank">Matteo Leonetti</a> (University of Leeds), <a href="http://eecs.csuohio.edu/~szhang/" target="_blank">Shiqi Zhang</a> (Cleveland State University), <a href="https://www.cs.tufts.edu/~jsinapov" target="_blank">Jivko Sinapov</a> (Tufts University), <a href="http://maxwellsvetlik.com/">Maxwell Svetlik</a>, and <a href="http://www.cs.utexas.edu/~pato/">Pato Lankenau</a> (Apple, Inc.)</p>
+
+<h2 id="contact">Contact Us</h2>
+
+Professor Peter Stone (Principal Investigator)<br>
+<a href="http://www.cs.utexas.edu" target="_top"> Department of Computer Science</a> <br>
+<a href="http://www.utexas.edu" target="_top"> The University of Texas at Austin</a> <br>
+<i><a href="mailto:pstone@cs.utexas.edu"> pstone @ cs.utexas.edu </a><br></i>
+<a href="http://www.cs.utexas.edu/~pstone" target="_top">www.cs.utexas.edu/~pstone</a> <br>
+
