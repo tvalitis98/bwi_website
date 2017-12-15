178178ca401e3f3cca563906d18cbabdaea30e84
tvalitis98 <tiffany.valitis@gmail.com>
2017-12-15 05:12:43 -0600

    Refactored uptime.php and robot_alive.php.

--- www/html/robot_alive.php
+++ www/html/robot_alive.php
@@ -1,71 +1,56 @@
 <html>
     <body>
-	robot_name: <?php echo $_GET["robot_name"]; ?><br>
-	timeout: <?php echo $_GET["timeout"]; ?><br>
+    robot_name: <?php
+echo $_GET["robot_name"];
+?><br>
+    timeout: <?php
+echo $_GET["timeout"];
+?><br>
 
-	<?php
-		require_once __DIR__ . '/create_event.php';
-		echo "$output";
-		$servername = "localhost";
-		$username = "webuser";
-		$password = "i_am_a_web_user";
+    <?php
+require_once __DIR__ . '/create_event.php';
+echo "$output";
+$servername = "localhost";
+$username   = "webuser";
+$password   = "i_am_a_web_user";
 
-		$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password);
 
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-		$select_query =
-			"SELECT robot_name, start_time, end_time " .
-			"FROM ROBOTS.ROBOT_SESSIONS " .
-			"WHERE " .
-			"robot_name='" . $_GET["robot_name"] . "' AND " .
-			"end_time > '" . (time()) . "';"
-			;
+$select_query = "SELECT robot_name, start_time, end_time " .
+                "FROM ROBOTS.ROBOT_SESSIONS " . "WHERE " . 
+                "robot_name='" . $_GET["robot_name"] . "' AND " . 
+                "end_time > '" . (time()) . "';";
 
-		$result = $conn->query($select_query);
+$result = $conn->query($select_query);
 
-		if ($result->num_rows > 0) {
-			// output data of each row
-			#die("GOT TO UPDATE");
-			while($row = $result->fetch_assoc()) {
-				$start_time = $row["start_time"];
-				$end_time = $row["end_time"];
-			}
-			$new_end_time = (time() + $_GET["timeout"]);	
-			$update_query =
-				"UPDATE ROBOTS.ROBOT_SESSIONS SET " .
-				"end_time='" . ( $new_end_time ) . "' " .
-				"WHERE " .
-				"start_time='" . $start_time . "' AND " .
-				"robot_name='" . $_GET["robot_name"] . "'";
-			$conn->query($update_query);
-		} else {
-			#die("GOT TO INSERT");
-			$insert_query =
-				"INSERT INTO ROBOTS.ROBOT_SESSIONS" .
-				"(robot_name, start_time, end_time) " .
-				"VALUES(" .
-				"'" . $_GET["robot_name"] . "', " . 
-				"'" . time() . "', " . 
-				"'" . (time() + $_GET["timeout"]) . "'" .
-				")";
-			$conn->query($insert_query);
-		}
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $start_time = $row["start_time"];
+        $end_time   = $row["end_time"];
+    }
+    $new_end_time = (time() + $_GET["timeout"]);
+    $update_query = "UPDATE ROBOTS.ROBOT_SESSIONS SET " . 
+                    "end_time='" . ($new_end_time) . "' " .
+                    "WHERE " . "start_time='" . $start_time . "' AND " . 
+                    "robot_name='" . $_GET["robot_name"] . "'";
 
-		$conn->close();
+    $conn->query($update_query);
+} else {
+    $insert_query = "INSERT INTO ROBOTS.ROBOT_SESSIONS" .
+                    "(robot_name, start_time, end_time) " .
+                    "VALUES(" . "'" . $_GET["robot_name"] . 
+                    "', " . "'" . time() . "', " . "'" . 
+                    (time() + $_GET["timeout"]) . "'" . ")";
 
-		/*
+    $conn->query($insert_query);
+}
 
-		
-			if ($conn->query($insert_query) === TRUE) {
-			} else {
-				echo "Error: " . $conn->error . "<br>";
-			}
-
-		*/
-	?>
+$conn->close();
+?>
 
     </body>
 </html>
--- www/html/uptime.php
+++ www/html/uptime.php
@@ -1,111 +1,130 @@
 <div>
-    <?php require_once __DIR__ . '/quickstart.php';
+    <?php
+require_once __DIR__ . '/quickstart.php';
 
-    $servername = "localhost";
-    $username = "webuser";
-    $password = "i_am_a_web_user";
+$servername = "localhost";
+$username   = "webuser";
+$password   = "i_am_a_web_user";
 
-    $conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$day_of_week = date('w');
+$back_days   = abs(1 - date('w'));
+$hour        = date('H');
+$minute      = date('i');
+$second      = date('s');
+
+// loop through the two weeks we want to report on
+for ($num_week = 0; $num_week < 2; $num_week++, $back_days += 7) {
+    // 86400 is number of seconds in a day
+    // H is hours 0-23, i is minutes 0-59, s is seconds 0-59
+    // find the difference in seconds between the last now and the last monday
+    $seconds_difference = ($back_days * 86400) - ((9 - $hour) * 3600) + ($minute * 60) + $second;
+    // use the difference in seconds to create a datetime (monday at 9am)
+    $start_date         = new DateTime("-" . $seconds_difference . " seconds");
+    $start_sec          = $start_date->getTimestamp();
+    // 374400 is 4 days and 8 hours worth of seconds
+    // use the first datetime to find the second (friday at 5pm)
+    $end_sec            = $start_sec + 374400;
+    $end_date           = new DateTime("@" . $end_sec);
+    $end_date->setTimeZone($start_date->getTimeZone());
+    
+    // start printing the uptime report for this week
+    echo "<h3>";
+    // the most recent week
+    if ($num_week == 0) {
+        // if we are at the end of the week
+        if (($day_of_week == 5 && $hour >= 17) || $day_of_week > 5) {
+            echo "This week (" . $start_date->format('m/d') . ") the robots were up for ";
+        }
+        // if we are at the beginning of the week after
+        else if ($day_of_week == 0 || ($day_of_week == 1 && $hour < 9)) {
+            echo "Last week (" . $start_date->format('m/d') . ") the robots were up for ";
+        }
+        // if we are in the middle of the week
+        else {
+            echo "This week (" . $start_date->format('m/d') . ") the robots have been up for ";
+        }
     }
-
-    $day_of_week = date('w');
-    $back_days = abs(1 - date('w'));
-    /*if(($day_of_week == 5 && date('H') >= 17) || $day_of_week > 5 || $day_of_week == 0) {
-        echo "GO BACK TO MOST RECENT";
+    // the second most recent week with some extra week before the target time starts
+    else if ($day_of_week == 0 || ($day_of_week == 1 && $hour < 9)) {
+        echo "Two weeks ago (" . $start_date->format('m/d') . ") the robots were up for ";
     }
+    // the second most recent week without the extra time before the target time starts
     else {
-        echo "GO BACK TWICE";
-        $back_days += 7;
-    }*/
-    for($num_week = 0; $num_week < 2; $num_week++, $back_days += 7) {
-	    // 86400 is number of seconds in a day
-	    // H is hours 0-23, i is minutes 0-59, s is seconds 0-59
-	    $seconds_difference = ($back_days * 86400) - ((9 - date('H')) * 3600) + (date('i') * 60) + date('s');
-	    $start_date = new DateTime("-" . $seconds_difference . " seconds");
-	    $start_sec = $start_date->getTimestamp();
-	    // 374400 is 4 days and 8 hours worth of seconds
-	    $end_sec = $start_sec + 374400;
-	    $end_date = new DateTime("@" . $end_sec);
-	    $end_date->setTimeZone($start_date->getTimeZone());
-
-            echo "<h3>";
-	    if($num_week == 0) {
-		    if(($day_of_week == 5 && date('H') >= 17) || $day_of_week > 5) {
-			echo "This week (" . $start_date->format('m/d') . ") the robots were up for ";
-	    	    }
-                    else if($day_of_week == 0 || ($day_of_week == 1 && date('H') < 9)) {
-                        echo "Last week (" . $start_date->format('m/d') . ") the robots were up for ";
-                    }
-	    	    else {
-			echo "This week (" . $start_date->format('m/d') . ") the robots have been up for ";
-	    	    }
-	    }
-	    else {
-		echo "Last week (" . $start_date->format('m/d') . ") the robots were up for ";
-	    }
-
-	    // echo $start_date->format("D, d M Y H:i:s O");
-	    // echo $end_date->format("D, d M Y H:i:s O");
-
-	    $select_query =
-		"SELECT robot_name, start_time, end_time " .
-		"FROM ROBOTS.ROBOT_SESSIONS " .
-		"WHERE " .
-		"start_time < '" . $end_sec . "' AND " .
-		"end_time > '" . $start_sec .
-		"' ORDER BY start_time;";
-
-	    $result = $conn->query($select_query);
-
-	    if ($result->num_rows > 0) {
-		$favorable_minutes = 0;
-		// loop through each day of the week monday-friday
-		for($num_day = 0; $num_day < 5; $num_day++) {
-		    // 480 minutes in 8 hours
-		    $day_array = array_fill(0, 480, false);
-		    while($row = $result->fetch_assoc()) {
-		        $start_time = round(($row["start_time"] - $start_sec - ($num_day * 86400)) / 60);
-		        $end_time = round(($row["end_time"] - $start_sec - ($num_day * 86400)) / 60);
-		        for($min = $start_time; $min < 480 && $min < $end_time; $min++) {
-		            $day_array[$min] = true;
-		        }
-		        // echo "<p>$start_time - $end_time</p>";
-		    }
-		    $result->data_seek(0);
-		    for($x = 0; $x < 480; $x++) {
-		        if($day_array[$x])
-		            $favorable_minutes++;
-		    }
-		}
-                echo "$favorable_minutes minutes, or ";
-                if($num_week == 0 && (($day_of_week == 5 && date('H') < 17)) || ($day_of_week == 1 && date('H') >= 9) || ($day_of_week > 1 && $day_of_week < 5)) {
-                    $effective_day_of_week = $day_of_week - 1;
-                    $hour_diff = 0;
-                    $min_diff = 0;
-                    if(date('H') >= 17) {
-			$effective_day_of_week++;
-                    }
-                    else if (date('H') > 9){
-                        $hour_diff = date('H') - 9;
-                        $min_diff = date('i');
-                    }
-                    $total_minutes = ($effective_day_of_week * 480) + ($hour_diff * 60) + $min_diff;
-		    echo number_format(100 * ($favorable_minutes / $total_minutes), 2);
-                }
-                else {
-                    echo number_format($favorable_minutes / 24.00, 2);
-                }
-	    }
-            else {
-		echo "0";
-	    }
-	    echo "% of the targetted time (Mon-Fri 9am-5pm).</h3>";
+        echo "Last week (" . $start_date->format('m/d') . ") the robots were up for ";
     }
-    $conn->close();
     
-    ?>
+    // query for all uptime that could possibly apply for the current week
+    $select_query = "SELECT robot_name, start_time, end_time " . 
+                    "FROM ROBOTS.ROBOT_SESSIONS " . "WHERE " . 
+                    "start_time < '" . $end_sec . "' AND " . 
+                    "end_time > '" . $start_sec . 
+                    "' ORDER BY start_time;";
     
+    $result = $conn->query($select_query);
+    
+    if ($result->num_rows > 0) {
+        $favorable_minutes = 0;
+        // loop through each day of the week monday-friday
+        for ($num_day = 0; $num_day < 5; $num_day++) {
+            // 480 minutes in 8 hours
+            // create an array to represent all the minutes in the target 8 hours
+            $day_array = array_fill(0, 480, false);
+            // loop through the query results 
+            while ($row = $result->fetch_assoc()) {
+                // adjuest start_time and end_time to be indexed into the array relative to the current day
+                $start_time = round(($row["start_time"] - $start_sec - ($num_day * 86400)) / 60);
+                $end_time   = round(($row["end_time"] - $start_sec - ($num_day * 86400)) / 60);
+                for ($min = $start_time; $min < 480 && $min < $end_time; $min++) {
+                    $day_array[$min] = true;
+                }
+            }
+            $result->data_seek(0);
+            // loop through the array we made in order to see what minutes we had robots active
+            for ($x = 0; $x < 480; $x++) {
+                if ($day_array[$x])
+                    $favorable_minutes++;
+            }
+        }
+        echo "$favorable_minutes minutes, or ";
+        // mid week
+        if ($num_week == 0 && (($day_of_week == 5 && $hour < 17)) ||
+                                ($day_of_week == 1 && $hour >= 9) ||
+                                ($day_of_week > 1 && $day_of_week < 5)) {
+            // find the difference in hours and minutes between now and the start of today
+            $hour_diff             = 0;
+            $min_diff              = 0;
+            // if past 5pm the difference is one target day
+            if ($hour >= 17) {
+                $hour_diff = 8;
+            }
+            // between 9am and 5pm
+            else if ($hour > 9) {
+                $hour_diff = $hour - 9;
+                $min_diff  = $minute;
+            }
+            // calculate the total number of minutes the robots should have been up thus far
+            $total_minutes = (($day_of_week - 1) * 480) + ($hour_diff * 60) + $min_diff;
+            echo number_format(100 * ($favorable_minutes / $total_minutes), 2);
+        }
+        // not mid week (total minutes always 2400)
+        else {
+            echo number_format($favorable_minutes / 24.00, 2);
+        }
+    }
+    // robots were never on!
+    else {
+        echo "0";
+    }
+    echo "% of the targetted time (Mon-Fri 9am-5pm).</h3>";
+}
+$conn->close();
+
+?>
+   
 </div>
